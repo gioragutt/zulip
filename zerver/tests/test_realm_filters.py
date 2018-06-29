b63d9a4369e4b04af11c5ff196db4ae66457bbf5
Giora Guttsait <giora111@gmail.com>
2018-06-29 17:21:18 +0300

    Fix filters and RTL

--- zerver/tests/test_realm_filters.py
+++ zerver/tests/test_realm_filters.py
@@ -5,6 +5,10 @@ from zerver.lib.test_classes import ZulipTestCase
 from zerver.models import RealmFilter
 
 class RealmFilterTest(ZulipTestCase):
+    BLANK_FIELD_ERROR = 'This field cannot be blank.'
+    INVALID_PATTERN_ERROR = 'Invalid filter pattern, you must use the following format OPTIONAL_PREFIX(?P<id>.+)'
+    INVALID_URL_ERROR = 'Enter a valid URL.'
+    INVALID_URL_FORMAT_ERROR = 'URL format string must be in the following format: `https://example.com/%(\\w+)s`'
 
     def test_list(self) -> None:
         email = self.example_email('iago')
@@ -19,39 +23,48 @@ class RealmFilterTest(ZulipTestCase):
         self.assertEqual(200, result.status_code)
         self.assertEqual(len(result.json()["filters"]), 1)
 
+    def assert_filter_not_created(self, pattern: str, url_format_string: str, error: str) -> None:
+        data = {"pattern": pattern, "url_format_string": url_format_string}
+        result = self.client_post("/json/realm/filters", info=data)
+        self.assert_json_error(result, error)
+
+    def assert_filter_created(self, pattern: str, url_format_string: str) -> None:
+        data = {"pattern": pattern, "url_format_string": url_format_string}
+        result = self.client_post("/json/realm/filters", info=data)
+        self.assert_json_success(result)
+
     def test_create(self) -> None:
         email = self.example_email('iago')
         self.login(email)
-        data = {"pattern": "", "url_format_string": "https://realm.com/my_realm_filter/%(id)s"}
-        result = self.client_post("/json/realm/filters", info=data)
-        self.assert_json_error(result, 'This field cannot be blank.')
 
-        data['pattern'] = '$a'
-        result = self.client_post("/json/realm/filters", info=data)
-        self.assert_json_error(result, 'Invalid filter pattern, you must use the following format OPTIONAL_PREFIX(?P<id>.+)')
+        self.assert_filter_not_created(pattern="",
+                                       url_format_string="https://realm.com/my_realm_filter/%(id)s",
+                                       error=self.BLANK_FIELD_ERROR)
 
-        data['pattern'] = 'ZUL-(?P<id>\d++)'
-        result = self.client_post("/json/realm/filters", info=data)
-        self.assert_json_error(result, 'Invalid filter pattern, you must use the following format OPTIONAL_PREFIX(?P<id>.+)')
+        self.assert_filter_not_created(pattern="$a",
+                                       url_format_string="https://realm.com/my_realm_filter/%(id)s",
+                                       error=self.INVALID_PATTERN_ERROR)
 
-        data['pattern'] = 'ZUL-(?P<id>\d+)'
-        data['url_format_string'] = '$fgfg'
-        result = self.client_post("/json/realm/filters", info=data)
-        self.assert_json_error(result, 'Enter a valid URL.')
+        self.assert_filter_not_created(pattern='ZUL-(?P<id>\d++)',
+                                       url_format_string="https://realm.com/my_realm_filter/%(id)s",
+                                       error=self.INVALID_PATTERN_ERROR)
 
-        data['pattern'] = 'ZUL-(?P<id>\d+)'
-        data['url_format_string'] = 'https://realm.com/my_realm_filter/'
-        result = self.client_post("/json/realm/filters", info=data)
-        self.assert_json_error(result, 'URL format string must be in the following format: `https://example.com/%(\\w+)s`')
+        self.assert_filter_not_created(pattern='ZUL-(?P<id>\d+)',
+                                       url_format_string='$fgfg',
+                                       error=self.INVALID_URL_ERROR)
 
-        data['url_format_string'] = 'https://realm.com/my_realm_filter/%(id)s'
-        result = self.client_post("/json/realm/filters", info=data)
-        self.assert_json_success(result)
+        self.assert_filter_not_created(pattern='ZUL-(?P<id>\d+)',
+                                       url_format_string='https://realm.com/my_realm_filter/',
+                                       error=self.INVALID_URL_FORMAT_ERROR)
 
-        data['pattern'] = 'ZUL2-(?P<id>\d+)'
-        data['url_format_string'] = 'https://realm.com/my_realm_filter/?value=%(id)s'
-        result = self.client_post("/json/realm/filters", info=data)
-        self.assert_json_success(result)
+        self.assert_filter_created(pattern='ZUL-(?P<id>\d+)',
+                                   url_format_string='https://realm.com/my_realm_filter/%(id)s')
+
+        self.assert_filter_created(pattern='ZUL2-(?P<id>\d+)',
+                                   url_format_string='https://realm.com/my_realm_filter/?value=%(id)s')
+
+        self.assert_filter_created(pattern='ZUL3-(?P<id>\d+)',
+                                   url_format_string='https://realm.com/#/my_realm_filter/?value=%(id)s')
 
     def test_not_realm_admin(self) -> None:
         email = self.example_email('hamlet')
